f7f81cfd9ddd5d7132ede4651a85701c2d23032c
TianZheng <zhengtsh095@gmail.com>
2023-06-30 10:43:20 +0200

    updated test file

--- Openscenario2Commonroad/test/OSC_Convert2_CR.py
+++ Openscenario2Commonroad/test/OSC_Convert2_CR.py
@@ -3,12 +3,12 @@ from osc_cr_converter.utility.configuration import ConverterParams
 from osc_cr_converter.utility.visualization import visualize_scenario
 
 # scenario path and name
-openscenario = '' # replace by /.../OpenSCENARIO2SUMO/OpenscenarioFiles/SimpleCurve.xosc
+openscenario = '' # replace by /.../OpenSCENARIO2SUMO/Openscenario2Commonroad/OpenscenarioFiles/SimpleCurve.xosc
 
 
 # build configuration
 # config = ConverterParams()
-config = ConverterParams.load('') # replace by /.../OpenSCENARIO2SUMO/test/Store.yaml
+config = ConverterParams.load('') # replace by /.../OpenSCENARIO2SUMO/Openscenario2Commonroad/test/Store.yaml
 
 # convert OpenSCENARIO to CommonRoad
 converter = Osc2CrConverter(config)
